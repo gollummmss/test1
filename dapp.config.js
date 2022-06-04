849bc1bb2e64aeab455f3671bc372e6f8768e4a1
gollummmss <106803423+gollummmss@users.noreply.github.com>
2022-06-04 19:29:02 +0100

    Update dapp.config.js

--- dapp.config.js
+++ dapp.config.js
@@ -1,10 +1,10 @@
 const config = {
   title: 'Gollum Town',
   description: 'For Serioous Investeours',
-  contractAddress: '0x8e642c89a45619d446c2a5eBc561e7310eAd6282',
-  maxMintAmount: 10,
+  contractAddress: '0x0c973709789F922A03C6dFb3998ab1d2b2b7B69c',
+  maxMintAmount: 30,
   presaleMaxMintAmount: 3,
-  price: 0.005
+  price: 0.0069
 }
 
 export { config }
