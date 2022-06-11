6c586f82b2092c891c22498eb1531eccd31dfc4b
gollummmss <106803423+gollummmss@users.noreply.github.com>
2022-06-12 00:20:50 +0100

    Update mint.js

--- pages/mint.js
+++ pages/mint.js
@@ -122,10 +122,10 @@ export default function Mint() {
   }
 
   return (
-    <div className="min-h-screen h-full w-full overflow-hidden flex flex-col items-center justify-center bg-brand-background ">
+    <div className="min-h-screen h-full w-full overflow-auto flex flex-col items-center justify-center bg-brand-background ">
       <div className="relative w-full h-full flex flex-col items-center justify-center">
         <img
-          src="/images/BG-1.JPG"
+          src="/images/web design demo.png"
           className="animate-pulse-slow absolute inset-auto block w-full min-h-screen object-cover"
         />
 
