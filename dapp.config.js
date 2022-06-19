586237c5cd8d8bda697dff1252e52639479b483b
gollummmss <106803423+gollummmss@users.noreply.github.com>
2022-06-19 10:11:12 +0100

    Update dapp.config.js

--- dapp.config.js
+++ dapp.config.js
@@ -3,7 +3,7 @@ const config = {
   description: 'BOOOOoooooOOONNGGGggggggz',
   contractAddress: '0x65866D8609D7971781b7A70B2beb7aba822cB1E3',
   maxMintAmount: 30,
-  presaleMaxMintAmount: 3,
+  presaleMaxMintAmount: 1,
   price: 0.0042
 }
 
